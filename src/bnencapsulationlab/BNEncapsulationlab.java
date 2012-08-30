527a0c601e1e509f780ab38b50e275d253bd91f6
bnowak8 <bnowak8@L11103038631.wctc.msft>
2012-08-30 13:52:16 -0500

    First Commit

---
+++ src/bnencapsulationlab/BNEncapsulationlab.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bnencapsulationlab;
+
+/**
+ *
+ * @author bnowak8
+ */
+public class BNEncapsulationlab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
