c382b3eb4d1997abab4e379e3e8b4d9cc9b748e0
bnowak8 <bnowak8@my.wctc.edu>
2012-08-30 14:01:41 -0500

    Update src/bnencapsulationlab/BNEncapsulationlab.java
    deleted comments

--- src/bnencapsulationlab/BNEncapsulationlab.java
+++ src/bnencapsulationlab/BNEncapsulationlab.java
@@ -1,19 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bnencapsulationlab;
 
-/**
- *
- * @author bnowak8
- */
+
 public class BNEncapsulationlab {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
 }
